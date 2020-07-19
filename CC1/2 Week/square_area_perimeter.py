f33ca6d1c764cdc772b72e74b7cdb1cabbb2f7de
kaory-china <45723567+kaory-china@users.noreply.github.com>
2020-07-19 15:18:12 -0300

    Update and rename 2 Week/calculo_perimetro_area_quadrado.py to CC1/2 Week/square_area_perimeter.py
    Amended file structure and added description of the exercise.

--- CC1/2 Week/square_area_perimeter.py
+++ CC1/2 Week/square_area_perimeter.py
@@ -1,3 +1,5 @@
+'it calculates square perimeter and area based on user int input of the square side size'
+
 MedidaLadoQuadrado = int(input("Digite o valor correspondente ao lado de um quadrado:"))
 Perimetro = MedidaLadoQuadrado * 4
 Area = MedidaLadoQuadrado ** 2
