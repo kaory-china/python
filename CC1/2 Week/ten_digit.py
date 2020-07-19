0f78f5e213ea8ba55755eef517d6f0022c07c131
kaory-china <45723567+kaory-china@users.noreply.github.com>
2020-07-19 15:24:04 -0300

    Update and rename 2 Week/opc3.py to CC1/2 Week/ten_digit.py
    Amended file structure and added description of the exercise.

--- CC1/2 Week/ten_digit.py
+++ CC1/2 Week/ten_digit.py
@@ -1,3 +1,5 @@
+'it takes a int input and prints the ten digit'
+
 NumeroInteiro = int(input("Digite um número inteiro:")) 
 DigitoDezena = (NumeroInteiro // 10) % 10
 print("O dígito das dezenas é", DigitoDezena)
