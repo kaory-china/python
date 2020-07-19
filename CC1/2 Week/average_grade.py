28ecf5a398eedd9c4453f4a234b38eb89a0b836e
kaory-china <45723567+kaory-china@users.noreply.github.com>
2020-07-19 15:15:07 -0300

    Update and rename 2 Week/ex2.py to CC1/2 Week/average_grade.py
    Amendment of file structure and added description of the exercise.

--- CC1/2 Week/average_grade.py
+++ CC1/2 Week/average_grade.py
@@ -1,3 +1,5 @@
+'it calculates average students\' grades from 4 user int inputs'
+
 PrimeiraNota = int(input("Digite a primeira nota:"))
 SegundaNota = int(input("Digite a segunda nota:"))
 TerceiraNota = int(input("Digite a terceira nota:"))
